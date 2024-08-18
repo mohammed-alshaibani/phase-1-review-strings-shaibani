7f6f887366cec2115e73db918decbd872a4941ee
mohammed-alshaibani <20160129@su.edu.ye>
2024-08-18 14:38:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Flatiron School";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage =
+  "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+// let shortGreeting = "Welcome, " + currentUser[0] + currentUser.slice(1) + "!";
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+console.log(shortGreeting);
